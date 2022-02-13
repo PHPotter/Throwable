9d370d5e1564dcae82aa7690da40ddc432155770
jaypotter <jay@jaypotter.ca>
2022-02-12 19:05:51 -0500

    Add LogicException

---
+++ Potter/Throwable/Exception/Logic/LogicException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Potter\Throwable\Exception\Logic;
+
+use \LogicException as NativeLogicException;
+
+class LogicException extends NativeLogicException implements LogicExceptionInterface
+{
+
+}
